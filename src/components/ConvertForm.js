b084719b8c465406b047d35e025ca338601b7514
Jiwon Park <jiiwon0@gmail.com>
2017-08-11 15:01:45 +0900

    pass rates as props & build modal

--- src/components/ConvertForm.js
+++ src/components/ConvertForm.js
@@ -3,17 +3,68 @@ import { Image, Text, View } from 'react-native';
 import { Card, CardSection, Input, Button, Header, Confirm } from './common';
 import { connect } from 'react-redux';
 import { numberChange, numberClear, numberErase } from '../actions';
+import flagList from './FlagList';
 import Numpad from './Numpad';
 import CountryList from './CountryList';
 
 class ConvertForm extends Component {
-    state = { showFromModal: false, showToModal: false };
+    constructor(props){
+        super(props);
+        this.state = {
+            showFromModal: false, 
+            showToModal: false,
+            fromCurrency: 'CAD',
+            fromCurrencySelect: 'false',
+            toCurrency: 'CAD',
+            toCurrencySelect: 'false',
+            currency: '',
+            serverAPI: {
+                base: 'CAD',
+                date: '2017-08-08',
+                rates: {
+                    "CAD": 1.0000,
+                    "AUD": 0.99666,
+                    "BGN": 1.3074,
+                    "BRL": 2.4653,
+                    "CHF": 0.76743,
+                    "CNY": 5.2999,
+                    "CZK": 17.458,
+                    "DKK": 4.9725,
+                    "GBP": 0.60455,
+                    "HKD": 6.1695,
+                    "HRK": 4.9492,
+                    "HUF": 203.09,
+                    "IDR": 10505,
+                    "ILS": 2.8563,
+                    "INR": 50.338,
+                    "JPY": 87.426,
+                    "KRW": 890.83,
+                    "MXN": 14.141,
+                    "MYR": 3.378,
+                    "NOK": 6.2705,
+                    "NZD": 1.0718,
+                    "PHP": 39.793,
+                    "PLN": 2.8423,
+                    "RON": 3.051,
+                    "RUB": 47.364,
+                    "SEK": 6.4292,
+                    "SGD": 1.0751,
+                    "THB": 26.273,
+                    "TRY": 2.7851,
+                    "USD": 0.78862,
+                    "ZAR": 10.539,
+                    "EUR": 0.66849,
+                }
+            }
+        }
+    }
 
     closeModal() {
         this.setState( this.state.showFromModal ? { showFromModal: false } : { showToModal: false })
     }
 
     render() {
+        console.log(flagList);
         const { currencyTextStyle, cardStyle } = styles;
         return (
             <View>
@@ -26,6 +77,8 @@ class ConvertForm extends Component {
                         <CountryList 
                             visible={this.state.showFromModal}
                             closeModal={this.closeModal.bind(this)}
+                            rates={this.state.serverAPI.rates}
+                            flagList={flagList}
                         />
                         {/*FromCurrency*/}
                         <Text style={currencyTextStyle}>
--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -10,73 +10,24 @@ import {
 import { connect } from 'react-redux';
 import { CardSection, Button, Header } from './common';
 import ListItem from './ListItem';
-import flagList from './FlagList';
 import { fetchRateService } from '../actions';
 
 class ConturyList extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            fromCurrency: 'CAD',
-            fromCurrencySelect: 'false',
-            toCurrency: 'CAD',
-            toCurrencySelect: 'false',
-            currency: '',
-            serverAPI: {
-                base: 'CAD',
-                date: '2017-08-08',
-                rates: {
-                    "CAD": 1.0000,
-                    "AUD": 0.99666,
-                    "BGN": 1.3074,
-                    "BRL": 2.4653,
-                    "CHF": 0.76743,
-                    "CNY": 5.2999,
-                    "CZK": 17.458,
-                    "DKK": 4.9725,
-                    "GBP": 0.60455,
-                    "HKD": 6.1695,
-                    "HRK": 4.9492,
-                    "HUF": 203.09,
-                    "IDR": 10505,
-                    "ILS": 2.8563,
-                    "INR": 50.338,
-                    "JPY": 87.426,
-                    "KRW": 890.83,
-                    "MXN": 14.141,
-                    "MYR": 3.378,
-                    "NOK": 6.2705,
-                    "NZD": 1.0718,
-                    "PHP": 39.793,
-                    "PLN": 2.8423,
-                    "RON": 3.051,
-                    "RUB": 47.364,
-                    "SEK": 6.4292,
-                    "SGD": 1.0751,
-                    "THB": 26.273,
-                    "TRY": 2.7851,
-                    "USD": 0.78862,
-                    "ZAR": 10.539,
-                    "EUR": 0.66849,
-                }
-            }
-        }
-    }
+    
+    // componentDidMount(){
+    //     this.props.fetchRateService();
+    // }
 
-    componentDidMount(){
-        this.props.fetchRateService();
-    }
-
-    renderItemList() {
+    renderItemList() { 
         return(
             <ScrollView>
-                {Object.keys(this.state.serverAPI.rates).map((currencyCode) => {
+                {this.props.rates && Object.keys(this.props.rates).map((currencyCode) => {
                     return (
-                        <TouchableOpacity onPress={() => this.countryPress(currencyCode)}>
+                        <TouchableOpacity key={currencyCode}> 
                             <CardSection style={{borderRadius: 4, borderWidth: 0.5, borderColor: '#d6d7da', height: 130}}>
-                                <Image style={{alignSelf: 'flex-end'}} source={flagList[currencyCode].flag}/>
-                                <Text style={{alignSelf: 'flex-end'}}>{currencyCode}</Text>
-                                <Text style={{paddingLeft: 20, fontSize: 18, paddingTop: 15}}>{flagList[currencyCode].label}</Text>
+                                <Image style={{alignSelf: 'flex-end'}} source={this.props.flagList[currencyCode].flag}/>   
+                                <Text style={{paddingLeft: 20, fontSize: 18, paddingTop: 15}}>{this.props.flagList[currencyCode].label}</Text>
+                                <Text style={{alignSelf: 'flex-end', fontSize: 14, paddingLeft: 5 }}>{ '('+currencyCode+')'}</Text>  
                             </CardSection>
                         </TouchableOpacity>
                     );
@@ -85,13 +36,13 @@ class ConturyList extends Component {
         )
     }
 
-    countryPress(currencyCode) { 
-        if(this.state.fromCurrency) {
-            this.setState({ fromCurrency: currencyCode, fromCurrencySelect: !this.state.fromCurrencySelect })
-        } else {
-            this.setState({ toCurrency: currencyCode, toCurrencySelect: !this.state.toCurrencySelect })
-        }
-    }
+    // countryPress(currencyCode) { 
+    //     if(this.state.fromCurrency) {
+    //         this.setState({ fromCurrency: currencyCode, fromCurrencySelect: !this.state.fromCurrencySelect }, key={currencyCode})
+    //     } else {
+    //         this.setState({ toCurrency: currencyCode, toCurrencySelect: !this.state.toCurrencySelect })
+    //     }
+    // }
         
         // this.setState(startCurrency ? {startCurrency: countryDollarCode, startCurrencySelecting: false} : {toCurrency: countryDollarCode, toCurrencySelecting: false})} 
         //     key={countryDollarCode} style={[{height: 50, alignItems: 'center', justifyContent: 'center'}, 
@@ -100,7 +51,7 @@ class ConturyList extends Component {
     
     
     render() {
-        const { visible, closeModal } = this.props;
+        const { visible, closeModal, rates, flagList } = this.props;
         return (
             <Modal
                 visible={visible}
@@ -110,7 +61,7 @@ class ConturyList extends Component {
             >
             <View>
                 <Header HeaderText={"Country List"} />
-                    {this.renderItemList()}
+                    {this.renderItemList()} 
             </View>
             
                 
